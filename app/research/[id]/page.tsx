7e798972cedd34fa656f171252d86fe75c1778d0
Augah2001 <124095439+Augah2001@users.noreply.github.com>
2025-02-10 19:04:45 +0000

    another 1

--- app/research/[id]/page.tsx
+++ app/research/[id]/page.tsx
@@ -26,19 +26,19 @@ const Page = ({ params: { id } }: { params: { id: string } }) => {
   }, [id]);
   return (
     <div className="">
-      <div className="bg-[#223525] flex justify-center   pt-28 pb-8">
+      <div className="bg-gray-900 flex justify-center   pt-28 pb-8">
         <div className="w-[70%]  ">
-          <h1 className="text-2xl font-normal text-[#bde5c5] ">
+          <h1 className="text-2xl font-normal text-[#D4AF37] ">
             {" "}
             {research.title}
           </h1>
           <div className=" flex justify-between">
-              <h1 className="text-sm mt-1 font-thin text-[#bde5c5] pt-1 ">
+              <h1 className="text-sm mt-1 font-thin text-[#fcf5dd] pt-1 ">
                 {" "}
                 {research.author}
               </h1>
               
-                <button className=" bg-yellow-300 transition-transform shadow-xl ease-in-out duration-500
+                <button className=" bg-yellow-400 transition-transform shadow-xl ease-in-out duration-500
                   transform hover:scale-105
                   text-[#223525] px-2 py-2 "
                   disabled={typeof research.documentUrl !== "string"}
@@ -51,7 +51,7 @@ const Page = ({ params: { id } }: { params: { id: string } }) => {
         </div>
       </div>
       <div className="relative h-[120px]">
-        <div className="bg-[#223525] h-[80px]"></div>
+        <div className="bg-gray-900 h-[80px]"></div>
 
         <div className=" absolute top-0 flex justify-center   w-[100%]">
           <div className=" bg-yellow-50">
@@ -65,25 +65,25 @@ const Page = ({ params: { id } }: { params: { id: string } }) => {
           </div>
         </div>
 
-        <div className="md:h-[431px] lg:h-[550px] sm:h-[180px] xs:h-[134px] bg-[#bde5c5]"></div>
-        <div className="bg-white ">
+        <div className="md:h-[431px] lg:h-[550px] sm:h-[180px] xs:h-[134px] bg-gray-800"></div>
+        <div className="bg-gray-900 ">
           <div className=" flex justify-center">
             <div className="w-[70%]">
               <div className="flex justify-center">
-                <h1 className=" xs:mt-20 py-3 text-2xl font-normal text-[#223525] ">
+                <h1 className=" xs:mt-20 py-3 text-2xl font-normal text-[#D4AF37] ">
                   ABSTRACT
                 </h1>
               </div>
-              <div className="flex mt-2 justify-center">
-                <h1 className="text-lg font-normal text-[#223525] ">
+              <div className="flex mt-2 justify-center bg-gray-900">
+                <h1 className="text-lg font-normal text-[#fcf5dd] ">
                   {research?.abstract}
                 </h1>
               </div>
-              <div className="h-[2px] mt-6 w-full bg-[#223525]"></div>
+              <div className="h-[2px] mt-6 w-full bg-yellow-400"></div>
               <div className="flex mt-3 mb-8 justify-center">
-                <button className=" bg-[#223525] transition-colors ease-in-out duration-400
-                 hover:bg-yellow-300 hover:text-[#223525]
-                  text-yellow-300 px-4 py-2  mt-4"
+                <button className=" bg-yellow-400 transition-colors ease-in-out duration-400
+                 hover:bg-yellow-300 hover:text-gray-800
+                  text-gray-800 px-4 py-2  mt-4"
                   disabled={typeof research.documentUrl !== "string"}
                   onClick={async()=> {
                     console.log(research?.documentUrl)
