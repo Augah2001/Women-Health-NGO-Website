59d5a6fb2821dfb56d2882ca5e28b114fd70b2c6
Augah2001 <124095439+Augah2001@users.noreply.github.com>
2025-02-17 18:14:45 +0000

    Pending changes exported from your codespace

--- app/Components/Menu.tsx
+++ app/Components/Menu.tsx
@@ -12,14 +12,14 @@ interface LinkItem {
 
 const Menu = ({ linkItems }: MenuProps) => {
   return (
-    <div className="h-full w-full" suppressHydrationWarning = {true}>
-      <ul suppressHydrationWarning = {true} className="absolute right-0  mt-2 w-48 bg-[#F3F5E7] hover:bg-[#e8edce] border border-gray-200  shadow-lg">
+    <div className="h-full rounded-md w-full" suppressHydrationWarning = {true}>
+      <ul suppressHydrationWarning = {true} className="absolute right-0 rounded-md  mt-2 w-48 bg-[#f5f2e7]  border border-gray-200  shadow-lg">
         {linkItems.map((item) => (
           <li suppressHydrationWarning = {true}
             key={item.label}
-            className='" px-4 py-4 text-gray-800 font-normal text-sm bg-[#F3F5E7] hover:bg-[#deedce]  cursor-pointer"'
+            className='" px-4 py-4 text-gray-800 font-normal text-sm bg-[#f5f4e7]  cursor-pointer"'
           >
-            <Link suppressHydrationWarning = {true} className="text-sm hover:text-green-500" href={item.href}>{item.label}</Link>
+            <Link suppressHydrationWarning = {true} className="text-sm hover:text-yellow-600" href={item.href}>{item.label}</Link>
           </li>
         ))}
       </ul>
